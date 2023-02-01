918e3c221de951b20eeff948b5185558d38d43d1
RoshniRaut <Roshni.Raut@ness.com>
2023-02-01 10:10:17 +0530

    initial commit

---
+++ Frontend/controllers/loginController.js
@@ -0,0 +1,28 @@
+
+app.controller('loginController',function($scope,$location,authFact,employeeService){
+      $scope.login=function(user){
+        
+        employeeService.getEmployees().then(function(result){
+          let found=result.data.find(e=> e.email==user.email)
+          console.log(result.data)
+          if(found){
+            if(found.password!=user.password)
+              alert("invalid password");
+              else{
+                //verify and set token
+                authFact.setAccessToken("1234")
+                $location.path("/home");
+              }
+          }
+          else
+            alert("user not registered!!!")
+        })
+      }
+
+})
+    
+app.service('employeeService',function($http){
+    this.getEmployees=function(){
+      return $http.get("http://0.0.0.0:9090/employees")
+    }
+})
